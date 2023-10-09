ee473abd61041155f2c914522991406d95c9a178
Onuh Uche <onuhblaze@gmail.com>
2023-10-09 15:48:04 +0100

    payment

--- src/scenes/Payment.jsx
+++ src/scenes/Payment.jsx
@@ -3,11 +3,19 @@ import { Container, Row, Col } from "reactstrap";
 import { useParams } from "react-router-dom";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
+import "../styles/payment.css";
+import { PaystackButton } from "react-paystack";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+const apiKey = process.env.REACT_APP_PAYSTACK_API_KEY;
+console.log(apiKey);
 
 const Payment = () => {
+  const navigate = useNavigate();
   const [order, setOrder] = useState({});
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
@@ -31,7 +39,50 @@ const Payment = () => {
       console.log(err);
     }
   }, []);
-  console.log(order);
+
+  const config = {
+    reference: new Date().getTime().toString(),
+    email: order && order.email,
+    name: order && order.name,
+    amount: order && order.totalAmount * 100, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+    publicKey: apiKey,
+  };
+
+  const handlePaystackSuccessAction = async (reference) => {
+    try {
+      // Update the order in the database with the paymentId
+      const orderRef = doc(db, "orders", id);
+
+      await updateDoc(orderRef, { paymentId: reference.reference });
+
+      setOrder((prevOrder) => ({
+        ...prevOrder,
+        paymentId: reference.reference,
+      }));
+
+      toast.success("Payment successful");
+
+      // redirect
+      setTimeout(() => {
+        navigate("/");
+      }, [3000]);
+    } catch (error) {
+      toast.error("Error updating order:", error);
+    }
+  };
+
+  // you can call this function anything
+  const handlePaystackCloseAction = () => {
+    // implementation for  whatever you want to do when the Paystack dialog closed.
+    console.log("closed");
+  };
+
+  const componentProps = {
+    ...config,
+    text: "Pay Now",
+    onSuccess: (reference) => handlePaystackSuccessAction(reference),
+    onClose: handlePaystackCloseAction,
+  };
 
   return (
     <Helmet title="Payment">
@@ -40,7 +91,36 @@ const Payment = () => {
       <section>
         <Container>
           <Row>
-            <Col></Col>
+            <Col lg="12">
+              {loading ? (
+                <LoadingSpinner />
+              ) : (
+                <div className="payment__container w-full">
+                  <div>
+                    <p>
+                      Order name: <span>{order.name}</span>
+                    </p>
+                    <p>
+                      Order email: <span>{order.email}</span>
+                    </p>
+                    <p>
+                      Order contact number: <span>{order.number}</span>
+                    </p>
+                    <p>
+                      Order id: <span>{id}</span>
+                    </p>
+                    <p>
+                      Total Price:{" "}
+                      <span>
+                        NGN {Intl.NumberFormat().format(order.totalAmount)}
+                      </span>
+                    </p>
+
+                    <PaystackButton {...componentProps} className="shop__btn" />
+                  </div>
+                </div>
+              )}
+            </Col>
           </Row>
         </Container>
       </section>
